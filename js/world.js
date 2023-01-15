b6c7993120bd472589885c7ff823cf224485207d
Oneirical <manchot@videotron.ca>
2023-01-15 00:06:21 -0500

    map: adaptative corridor placement

--- js/world.js
+++ js/world.js
@@ -119,15 +119,25 @@ class World{
             for(let j=0;j<9;j++){
                 if (worldgen[i][j].passable){
                     let roomType;
+                    let flip = false;
                     if (Math.random() < 0.6 && j < 8 && i < 8 && worldgen[i+1][j].passable && worldgen[i][j+1].passable && worldgen[i+1][j+1].passable){
                         roomType = GrandHallFaith;
                         worldgen[i+1][j] = new Wall(i+1,j);
                         worldgen[i][j+1] = new Wall(i,j+1);
                         worldgen[i+1][j+1] = new Wall(i+1,j+1);
                     }
+                    else if (j < 8 && j > 0 && i < 8 && i > 0 && worldgen[i][j+1].passable && worldgen[i][j-1].passable && !worldgen[i+1][j].passable && !worldgen[i-1][j].passable){
+                        roomType = shuffle([NarrowFaith,BridgeFaith])[0]; // find a way to place these on the edge of the map too without exploding everything?
+                    }
+                    else if (j < 8 && j > 0 && i < 8 && i > 0 && worldgen[i+1][j].passable && worldgen[i-1][j].passable && !worldgen[i][j+1].passable && !worldgen[i][j-1].passable){
+                        roomType = shuffle([NarrowFaith,BridgeFaith])[0];
+                        flip = true;
+                    }
                     else roomType = shuffle(this.roompool)[0];
                     this.rooms[i][j] = new roomType(44); //44 index is temporary
+                    if (flip) flipRoom(this.rooms[i][j].id);
                     this.rooms[i][j].insertRoom();
+                    if (flip) flipRoom(this.rooms[i][j].id);
                 }
                 else this.rooms[i][j] = "nan";
             }
