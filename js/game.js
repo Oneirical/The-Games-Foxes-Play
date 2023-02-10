81cfae510c6b91abae4cd27611d2cdd3d9943577
Oneirical <manchot@videotron.ca>
2023-02-09 23:41:12 -0500

    save: narrow down saving to player

--- js/game.js
+++ js/game.js
@@ -649,7 +649,7 @@ function printAtSidebar(text, size, x, y, color, lineHeight, fitWidth)
 
 function reloadGame(){
     let saveData = JSON.parse(localStorage["saves"]);
-    let reloadData = {1 : monsters, 2 : tiles, 3 : player, 4 : world, 5 : wheel, 6 : log};
+    //let reloadData = {1 : monsters, 2 : tiles, 3 : player, 4 : world, 5 : wheel, 6 : log};
     let reloader = function(player){
         Object.keys(player).forEach(function(key){
             player[key] = saveData[key] 
@@ -657,15 +657,12 @@ function reloadGame(){
         return player;
     }
     player = reloader(player);
-    //monsters = saveData[1];
-    //tiles = saveData[2];
-    //player = saveData[3];
      
 }
 
 function saveGame(){
     localStorage.clear();
-    let saveFile = {1 : monsters, 2 : tiles, 3 : player, 4 : world, 5 : wheel, 6 : log};
+    //let saveFile = {1 : monsters, 2 : tiles, 3 : player, 4 : world, 5 : wheel, 6 : log};
     const getCircularReplacer = () => {
         const seen = new WeakSet();
         return (key, value) => {
